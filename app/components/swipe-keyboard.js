2cefbc7c021f126931b75e16ee07f122c023512e
Cory <cory.forsyth@gmail.com>
2019-10-12 18:29:51 -0400

    use under-image of keyboard for easier grabbing of line

--- app/components/swipe-keyboard.js
+++ app/components/swipe-keyboard.js
@@ -1,10 +1,10 @@
 import Component from '@ember/component';
 
 const LINE_WIDTH = 1;
-const COLOR = { r: '100%', g: '100%', b: '100%', a: 1 };
+const COLOR = { r: 255, g: 255, b: 255, a: 1 };
 
 function setup(canvas) {
-  let img = canvas.querySelector('img');
+  let img = document.querySelector('#keyboard');
   let { width, height } = img;
   if (width === 0 || height === 0) {
     img.addEventListener('load', () => {
@@ -14,8 +14,6 @@ function setup(canvas) {
   }
   canvas.width = width;
   canvas.height = height;
-  let ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
   new DrawSurface(canvas).start();
 }
 
@@ -38,20 +36,37 @@ class DrawSurface {
       this.brush = new Brush(ctx);
       this.brush.touchstart(this._localPos(e));
     });
-    canvas.addEventListener('touchend', e => {
-      this.brush.touchend();
-      this.brush = null;
-    });
     canvas.addEventListener('touchmove', e => {
       this.brush.touchmove(this._localPos(e));
     });
+    canvas.addEventListener('touchend', () => {
+      this.brush.touchend();
+      this.brush = null;
+      drawOntoSurface(canvas);
+      // clearCanvas(canvas);
+    });
   }
+}
+
+function clearCanvas(canvas) {
+  let ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawOntoSurface(canvas) {
+  let surface = document.querySelector('#surface');
+  // clearCanvas(surface);
+
+  let ctx = surface.getContext('2d');
+  surface.width = canvas.width;
+  surface.height = canvas.height;
+  ctx.drawImage(canvas, 0, 0);
 }
 
 class Brush {
   constructor(ctx) {
     this.ctx = ctx;
-    this.ctx.globalCompositeOperation = 'source-over';
+    this.ctx.globalCompositeOperation = 'difference';
     this.prevPos = { x: null, y: null };
   }
 
